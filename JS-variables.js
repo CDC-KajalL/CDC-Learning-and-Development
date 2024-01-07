5841d1392a21451adc3e04a2a192c1d37991b7a7
Einsteinia <informationviakate2004@gmail.com>
2024-01-07 20:54:31 +0530

    added usefuls

--- JS-variables.js
+++ JS-variables.js
@@ -10,3 +10,24 @@ else{
     alert("Oops you loose!");
 }
 
+//conversions
+let str = "123";
+value = String(value); // now value is a string "true"
+let num2 = Number(str); // becomes a number 123
+alert( Boolean(1) ); // true
+alert( Boolean(0) ); // false
+alert( Boolean("hello") ); // true
+alert( Boolean("") ); // false
+
+// Exponentiation **
+alert( 2 ** 2 ); // 4 (2 * 2)
+alert( 2 ** 3 ); // 8 (2 * 2 * 2)
+alert( 2 ** 4 ); // 16 (2 * 2 * 2 * 2)
+
+// confirm
+// shows a message and waits for the user to press “OK” or “Cancel”. It returns true for OK and
+// false for Cancel/ Esc .
+
+let isBoss = confirm("Are you the boss?");
+alert( isBoss ); // true if OK is pressed
+
