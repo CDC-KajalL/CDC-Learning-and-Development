3152763bbfbe90837147342dbdf67589ed8a7e68
Einsteinia <informationviakate2004@gmail.com>
2024-01-07 17:21:32 +0530

    Alert prompt concepts

---
+++ JS-alerts.js
@@ -0,0 +1,4 @@
+// alert.log("hello"); //The alert() method is a method on the window object, which is only available in the browser.
+console.log("hello!");
+var name1 = prompt("What is your name?");
+alert(name1);
