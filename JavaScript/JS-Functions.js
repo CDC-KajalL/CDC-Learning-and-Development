ddc99947b15f8440a625be27361e9958463cb4d9
Einsteinia <informationviakate2004@gmail.com>
2024-01-08 22:21:34 +0530

    commit

---
+++ JavaScript/JS-Functions.js
@@ -0,0 +1,10 @@
+function showMessage(){
+    alert("Hare Krishna!")
+}
+// showMessage()
+function testResults (form) {
+    var inputValue = form.inputbox.value;
+    alert ("You typed: " + inputValue);
+}
+let sum = (a,b) => alert(a+b);
+//sum(2,3);
