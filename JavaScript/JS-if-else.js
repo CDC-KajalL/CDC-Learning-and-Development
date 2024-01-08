46db6e6709b423e904788efaf6dc331c484e1a36
Einsteinia <informationviakate2004@gmail.com>
2024-01-08 12:39:45 +0530

    Added JS if else  and conditional operator examples

--- JavaScript/JS-if-else.js
+++ JavaScript/JS-if-else.js
@@ -1,8 +1,18 @@
 let accessAllowed;
-let age = prompt('How old are you?', '');
+let age = prompt("How old are you?", "");
 if (age > 18) {
-accessAllowed = true;
+  accessAllowed = true;
 } else {
-accessAllowed = false;
+  accessAllowed = false;
 }
 alert(accessAllowed);
+//Conditional Operator 
+// let result = condition ? value1 : value2;
+let accessAllowed2 = (age > 18) ? true : false;
+// In the example above, you can avoid using the question mark operator because the
+// comparison itself returns true/false :
+let age2 = prompt("How old are you?", "");
+let accessAllowed3 = age2 > 18;
+age2 = (age2<=3) ? 'Hey cutie': (age2<15) ? 'Hey baby What\'s up': (age2<100) ? 'Hey there': 'OMG! Welcome Sir/Madam';
+alert(age2)
+
