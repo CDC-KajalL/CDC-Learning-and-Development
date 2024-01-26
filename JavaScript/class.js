f3447292fce2dc50c5289bf9f584429ba6313e50
Einsteinia <informationviakate2004@gmail.com>
2024-01-26 16:49:44 +0530

    commit

--- JavaScript/class.js
+++ JavaScript/class.js
@@ -1,9 +1,40 @@
-class rectangle{
-
+class Area{
+    constructor(length, width){
+        this.length = length;
+        this.width = width;
+        this.r = 0;
+    }
+    rectangle(){
+        this.r = this.length*this.width;
+        return this.r;
+    }
+    square(){
+        this.rectangle();
+        return this.r;
+    }
+    triangle(){
+        this.r = 1/2*this.length*this.width;
+        return this.r;
+    }
 }
-class square{
-
-}
-class trinangle{
-    
-}
+document.getElementById("calculate").addEventListener("click", ()=>{
+    let dd = document.getElementById("dropdown");
+    let index = dd.selectedIndex;
+    let selected = dd.options[index].value;
+    // let selected = dd.options[index].text;
+    let length = prompt("Enter length:")
+    let width = prompt("Enter width:")
+    const myObj = new Area(length, width);
+    if(selected == "square"){
+        let res = myObj.square();
+        alert(res)
+    }
+    else if(selected == "rectangle"){
+        let res = myObj.rectangle();
+        alert(res);
+    }
+    else if(selected == "triangle"){
+        let res = myObj.triangle();
+        alert(res);
+    }
+})
