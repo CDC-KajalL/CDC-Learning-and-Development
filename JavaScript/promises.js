8e8618fc5e56d40b10bde992d5308cf94f3841a3
Einsteinia <informationviakate2004@gmail.com>
2024-02-04 18:40:18 +0530

    promises added

---
+++ JavaScript/promises.js
@@ -0,0 +1,13 @@
+//fibonacci series
+function stepOne(value){
+    return new promise((resolve)=>{
+        let n = prompt("Enter length of the fibonacci series:")
+        resolve()
+    })
+}
+function stepTwo(value){
+
+}
+function stepThree(value){
+
+}
